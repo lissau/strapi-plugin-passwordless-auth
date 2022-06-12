4e895649ead724318e7874a059f443cabb96d715
Nicolai Lissau <nicolai.lissau@gmail.com>
2022-06-12 16:12:15 +0200

    Added responsive email with login link using mjml.io (https://mjml.io/try-it-live)

---
+++ mjml-email-template.js
@@ -0,0 +1,21 @@
+<mjml>
+  <mj-body background-color="#ECECEC">
+    <mj-section >
+      <mj-column padding="20px" background-color="white" border-radius="20px" >
+
+        <mj-image width="100px" src="https://via.placeholder.com/150"></mj-image>
+
+        <mj-divider border-width="1px" border-color="#319795"></mj-divider>
+
+        <mj-text padding="20px" align="center" font-size="22px" color="#404040" font-family="arial">Hej!</mj-text>
+        
+         <mj-text line-height="22px" font-size="18px" color="#404040" font-family="arial">Log ind på Cykelregistret ved at klikke på knappen herunder 👇</mj-text>
+        
+        <mj-button padding-top="40px" font-weight="600" inner-padding="20px 50px" font-size="16px" font-family="arial" background-color="#319795" color="white">
+          Log ind
+         </mj-button>
+
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
